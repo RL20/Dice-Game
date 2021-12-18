58c88d9090c56049608c27716dfe3b21b736ff2c
RL20 <harel_shalom@yahoo.com>
2021-12-18 17:37:11 +0200

    start-design

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -1,5 +1,11 @@
 import React from "react";
 import "../style/Dice.css";
+import a from "../images/dice-1.png";
+import b from "../images/dice-2.png";
+import c from "../images/dice-3.png";
+import d from "../images/dice-4.png";
+import e from "../images/dice-5.png";
+import f from "../images/dice-6.png";
 class Dice extends React.Component {
   constructor(props) {
     super(props);
@@ -7,7 +13,13 @@ class Dice extends React.Component {
   }
   render() {
     const { text } = this.props;
-    return <div>{text}</div>;
+    const diceList = { 1: a, 2: b, 3: c, 4: d, 5: e, 6: f };
+    return (
+      <div className="dice">
+        <img src={diceList[text]} alt="" />
+      </div>
+    );
+    // return <div>{text}</div>;
   }
 }
 
